02c200853ffd47907843609a6754f3cc00f18a9d
mahim23 <mahim23agrawal@gmail.com>
2018-03-30 17:31:07 +0530

    dashboard app

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  columnsToDisplay: ['name', 'releaseDate', 'rating'];
+  displayedColumns = ['name', 'releaseDate', 'rating', 'link'];
+  dataSource = new MatTableDataSource(movies);
+  pageSizeOptions = [2, 4, 10];
+  genres = ['All', 'Comedy', 'Horror', 'SciFi'];
+  selectedGenre = 'All';
   constructor() { }
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.filterPredicate = (data, filter) => {
+      return data.genre.toLowerCase() === filter;
+    };
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter() {
+    console.log(this.selectedGenre);
+    let genre = this.selectedGenre.trim();
+    genre = genre.toLowerCase();
+    if (genre !== 'all') {
+      this.dataSource.filter = genre;
+    } else {
+      this.dataSource.filter = '';
+    }
+  }
+
+  goToMovie(link) {
+    console.log(link);
+    console.log(this.dataSource);
+  }
 
   ngOnInit() {
   }
 
 }
+
+const movies = [
+  {
+    movie_id: 100,
+    name: 'Movie 1',
+    releaseDate: '20/20/2000',
+    rating: 9,
+    link: 'link1',
+    genre: 'Comedy'
+  },
+  {
+    movie_id: 101,
+    name: 'Movie 2',
+    releaseDate: '20/20/2000',
+    rating: 8,
+    link: 'link2',
+    genre: 'Horror'
+  },
+  {
+    movie_id: 102,
+    name: 'Movie 3',
+    releaseDate: '20/20/2000',
+    rating: 6,
+    link: 'link3',
+    genre: 'SciFi'
+  },
+  {
+    movie_id: 103,
+    name: 'Movie 4',
+    releaseDate: '20/20/2000',
+    rating: 7,
+    link: 'link4',
+    genre: 'Horror'
+  },
+  {
+    movie_id: 104,
+    name: 'Movie 5',
+    releaseDate: '20/20/2000',
+    rating: 9,
+    link: 'link5',
+    genre: 'Comedy'
+  }
+];
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, Router } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import {MatButtonModule, MatExpansionModule, MatInputModule, MatPaginatorModule, MatSelectModule, MatSortModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatTableModule, MatButtonModule, MatSortModule, MatInputModule, MatPaginatorModule, MatSelectModule,
+    MatExpansionModule,
     RouterModule.forChild([{
       path: '',
       component: DashboardComponent
